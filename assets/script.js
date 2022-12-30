e6bb24b39ceadeb6bf1566a3196b8853976c802e
Squirrely Rob <clemmer.robert.j@gmail.com>
2022-12-30 15:56:07 -0500

    fixed materialize form

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+//this bit makes the materialize CSS form work
+$(document).ready(function(){
+   $('select').formSelect();
+ });
+
+
 var states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 var listEl = document.getElementById('jobList');
 var countEl = document.getElementById('totalCount');
